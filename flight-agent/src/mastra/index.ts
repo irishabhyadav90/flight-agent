0fbdd0052e29fcbc956c4b0110aed82e623bd4f5
Rishabh Yadav <irishabhyadav90@gmail.com>
2025-12-24 21:22:19 +0400

    feat: Add conversation memory

--- flight-agent/src/mastra/index.ts
+++ flight-agent/src/mastra/index.ts
@@ -1,8 +1,14 @@
 import { Mastra } from '@mastra/core/mastra';
 import { flightAgent } from './agents/flight.agent';
+import { LibSQLStore } from "@mastra/libsql";
+
+const storage = new LibSQLStore({
+  url: "file:./storage.db",
+});
 
 export const mastra = new Mastra({
   agents: {
     flightAgent,
   },
+  storage
 });
